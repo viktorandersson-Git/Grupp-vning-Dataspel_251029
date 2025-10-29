a4a48cdda5866fe6903a0491af81c10e4c896755
CajsaMartensson <cajsa.maartensson@gmail.com>
2025-10-29 15:32:02 +0100

    Added a log in method (not done).

--- Gruppövning-Dataspel_251029/User.cs
+++ Gruppövning-Dataspel_251029/User.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gruppövning_Dataspel_251029
+﻿namespace Gruppövning_Dataspel_251029
 {
     internal class User
     {
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
 
         public int PIN { get; set; }
 
         //skapa log in
+        public static void LogIn()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("\nSkriv in ditt användarnamn:");
+                string writtenUsername = Console.ReadLine();
+
+                Console.Write("Skriv in din PIN-kod");
+                string writtenPIN = Console.ReadLine();
+
+                CheckPassword(writtenUsername, writtenPIN);
+            }
+
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+            
+        }
     }
 }
