48248e5393c86f4a543c76f13e2e1bf8f235af1a
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2025-10-29 15:31:06 +0100

    Created Consoles and added methods

--- Gruppövning-Dataspel_251029/Consoles.cs
+++ Gruppövning-Dataspel_251029/Consoles.cs
@@ -8,7 +8,39 @@ namespace Gruppövning_Dataspel_251029
 {
     internal class Consoles
     {
-
         //ska läsa library och initiera spel
-    }
+        public string Name { get; set; }
+        public bool IsOn { get; set; }
+        public string Game { get; set; }
+        public bool GameStarted { get; set}
+
+        public Consoles(string name, string game)
+        {
+            Name = name;
+            IsOn = false;
+            Game = "";
+        }
+
+        public void PowerOn()
+        {
+            IsOn = true;
+            Console.WriteLine($"{Name} is now turned on.");
+        }
+        public void PowerOff()
+        {
+            IsOn = false;
+            Console.WriteLine($"{Name} is now turned off.");
+        }
+
+        public void StartGame()
+        {
+            GameStarted = true;
+        }
+
+        if (Consoles.IsOn)
+        {
+
+        }
+
+}
 }
